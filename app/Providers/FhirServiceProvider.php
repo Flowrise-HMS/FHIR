475a26ad47bd3f68d2fff8370c1ad7e467f5066b
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-07-08 13:13:47 +0000

    feat(fhir): wire all transformers into FhirServiceProvider, add module deps

--- app/Providers/FhirServiceProvider.php
+++ app/Providers/FhirServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace Modules\FHIR\Providers;
 
+use Modules\FHIR\FhirRouting\FhirResourceRegistrar;
+use Modules\FHIR\FhirResponse\FhirResponseFactory;
+use Modules\FHIR\FhirSearch\SearchParameterParser;
+use Modules\FHIR\FhirSearch\SearchQueryBuilder;
+use Modules\FHIR\FhirSearch\PaginationHandler;
+use Modules\FHIR\FhirValidation\FhirValidator;
 use Nwidart\Modules\Support\ModuleServiceProvider;
 
 class FhirServiceProvider extends ModuleServiceProvider
@@ -22,5 +28,23 @@ class FhirServiceProvider extends ModuleServiceProvider
             'fhir.negotiation',
             \Modules\FHIR\Http\Middleware\FhirContentNegotiation::class
         );
+    }
+
+    public function register(): void
+    {
+        parent::register();
+
+        $this->app->singleton(FhirResourceRegistrar::class);
+        $this->app->singleton(FhirResponseFactory::class);
+        $this->app->singleton(FhirValidator::class, fn ($app) => new FhirValidator(module_path('FHIR', 'Resources/schemas')));
+        $this->app->singleton(SearchParameterParser::class);
+        $this->app->singleton(SearchQueryBuilder::class);
+        $this->app->singleton(PaginationHandler::class);
+
+        $this->app->resolving(FhirResourceRegistrar::class, function ($registrar) {
+            $registrar->register('Patient', \Modules\Patient\Classes\Fhir\FhirPatientTransformer::class);
+            $registrar->register('Practitioner', \Modules\Staff\Classes\Fhir\FhirPractitionerTransformer::class);
+            $registrar->register('PractitionerRole', \Modules\Staff\Classes\Fhir\FhirPractitionerRoleTransformer::class);
+        });
     }
 }
